65005241267f91cd32839df6efb73a1ea14e290a
Brice Boulesteix <brice.boulesteix@gmail.com>
2018-05-21 17:23:48 +0200

    Add seed database file to rails project
    * Created `seeds.rb` file to generate 10 fake users and 10 fake hideouts.
    * Added faker gem to `gemfile`.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_135742) do
+ActiveRecord::Schema.define(version: 2018_05_21_150001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2018_05_21_135742) do
     t.integer "safety"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_hideouts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,7 +39,7 @@ ActiveRecord::Schema.define(version: 2018_05_21_135742) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.string "name", default: "", null: false
+    t.string "name"
     t.text "about"
     t.string "avatar"
     t.datetime "created_at", null: false
@@ -46,4 +48,5 @@ ActiveRecord::Schema.define(version: 2018_05_21_135742) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "hideouts", "users"
 end
