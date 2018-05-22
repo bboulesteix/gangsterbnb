8d28a31ebf22d51ca231a1ad9508575208facda8
Basile Marquefave <basile.marquefave@gmail.com>
2018-05-22 12:42:06 +0200

    navbar & log

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_many :hideouts, dependent: :destroy
-  validates :name, presence: true
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
