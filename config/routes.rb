9fe8d97e5653fb6bf62bf94b2f37da14134bc676
Baptiste Lefranc-Morin <baptistelefrancmorin@gmail.com>
2018-05-23 11:45:43 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,4 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: [:index]
   root to: 'hideouts#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/dashboard'
 end
