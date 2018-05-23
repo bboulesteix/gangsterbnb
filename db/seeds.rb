55106103e24984f2ae000c2ee0a615b20f0a969f
Basile Marquefave <basile.marquefave@gmail.com>
2018-05-23 16:36:03 +0200

    card

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning database…'
+
+Hideout.destroy_all
+Booking.destroy_all
 User.destroy_all
 
 puts 'Creating users…'
