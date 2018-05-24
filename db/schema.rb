ea9f974775c64151d66d0b91094591f15c761a0d
baptiste lefranc morin <baptistelefrancmorin@gmail.com>
2018-05-24 16:21:09 +0200

    add rating to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_111306) do
+ActiveRecord::Schema.define(version: 2018_05_24_140003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2018_05_24_111306) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "rating"
     t.index ["hideout_id"], name: "index_reviews_on_hideout_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
